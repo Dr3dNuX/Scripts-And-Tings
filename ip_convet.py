6a0d4258e6154b89b56121c6f882c8a58e841d2f
Dr3dNuX <84602650+Dr3dNuX@users.noreply.github.com>
2022-01-24 17:15:41 -0700

    Update ip_convet.py

--- ip_convet.py
+++ ip_convet.py
@@ -1,4 +1,4 @@
-# a very simple script to convet an ip address to binary
+# a very simple script to convert an ip address to binary
 # and convert the mask to binairy as well
 
 
@@ -6,13 +6,13 @@
 ip_add = input("Enter ip: ")
 mask = input("Enter mask ")
 
-#splitting the strings using . delimitor then assigning the
-#octets to sepret varables
+#splitting the strings using . delimiter then assigning the
+#octets to separate variables
 
 A,B,C,D = ip_add.split(".")
 E,F,G,H = mask.split(".")
 
-#formatting and converting the strings to intagers then using
-#Binary conversio to convert the new intergers to binary
+#formatting and converting the strings to integers then using
+#Binary conversion to convert the new intergers to binary
 print("ip address: {:b}.{:b}.{:b}.{:b}".format(int(A),int(B),int(C),int(D)))
 print("Subnet mask: {:b}.{:b}.{:b}.{:b}".format(int(E),int(F),int(G),int(H)))
